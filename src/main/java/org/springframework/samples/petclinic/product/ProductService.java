7ebac15f5009fb5d752e82dedac2f138dcd4632f
josemabp1 <josemabp13@gmail.com>
2021-12-21 12:34:50 +0100

    Hechos tests del 1 al 7, 8 da eror (NullPointerException)

--- src/main/java/org/springframework/samples/petclinic/product/ProductService.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductService.java
@@ -1,22 +1,44 @@
 package org.springframework.samples.petclinic.product;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.samples.petclinic.pet.PetType;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
 public class ProductService {
+	
+	@Autowired
+	ProductRepository productRepo;
+	
     public List<Product> getAllProducts(){
-        return null;
+        return productRepo.findAll();
     }
 
+    @Transactional(readOnly = true)
+	public List<ProductType> findAllProductTypes() throws DataAccessException {
+		return productRepo.findAllProductTypes();
+	}
+    
     public List<Product> getProductsCheaperThan(double price) {
         return null;
     }
 
     public ProductType getProductType(String typeName) {
-        return null;
+        return productRepo.getProductType(typeName);
     }
 
     public Product save(Product p){
         return null;       
+    }
+    
+    public List<Product> getProductsCheaperThan(Double price) {    	
+    	return productRepo.findByPriceLessThan(price);
     }
 
     
